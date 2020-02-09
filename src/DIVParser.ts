190094c48f8afacc8010e5b8e91cc7f3b5b684e8
vii <vii@divero.net>
2020-02-09 15:27:15 +0100

    Removed WIP code

--- src/DIVParser.ts
+++
@@ -1,9 +0,0 @@
-import * as Parser from 'parser';
-import { DIVTokenizer } from './DIVTokenizer';
-
-export class DIVParser extends Parser {
-    constructor() {
-        super();
-        super.apply(this, new DIVTokenizer());
-    }
-}
--- src/DIVSignatureHelpProvider.ts
+++
@@ -1,8 +0,0 @@
-import * as vscode from 'vscode';
-
-export class DIVSignatureHelpProvider implements vscode.SignatureHelpProvider
-{
-    provideSignatureHelp(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.SignatureHelpContext): vscode.ProviderResult<vscode.SignatureHelp> {
-        throw new Error("Not implemented");
-    }    
-}
